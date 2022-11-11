cfae695cec1385eebc3e529a3bbd11708d2366a3
young-02 <75434044+young-02@users.noreply.github.com>
2022-11-11 18:45:06 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.py
@@ -0,0 +1,10 @@
+def solution(angle):
+    if angle == 180:
+        return 4
+    elif angle > 90 and angle <180:
+        return 3
+    elif angle == 90:
+        return 2
+    else: 
+        return 1
+    
