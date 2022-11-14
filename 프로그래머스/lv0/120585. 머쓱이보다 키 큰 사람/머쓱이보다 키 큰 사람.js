936a8f675476d1c776125c58e79bdf55f681314d
young-02 <75434044+young-02@users.noreply.github.com>
2022-11-14 16:21:15 +0900

    Update 머쓱이보다 키 큰 사람.js

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -7,3 +7,19 @@ function solution(array, height) {
     })
     return answer;
 }
+
+
+//다른사람 풀이 보충
+//1.filter
+function solution(array, height) {
+    var answer = array.filter(item => item > height).length;
+    return answer;
+}
+
+//2.forEach
+function solution(array, height) {
+    var answer = 0;
+    array.forEach(v=> {if(v>height) answer++;})
+    return answer;
+}
+
