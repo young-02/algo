e7340bf12e6a4d18d394332c08a1f5d59c896dfa
young-02 <75434044+young-02@users.noreply.github.com>
2022-11-14 16:14:33 +0900

    Update 양꼬치.js

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,10 +1,18 @@
 function solution(n, k) {
     var answer = 0;
     let ten = Math.floor(n / 10)
-
     if(!ten){
        answer =  n*12000 + k * 2000
     } 
     return answer = (n * 12000) + ((k - ten) * 2000)
        
 }
+
+
+//다른사람 풀이 보충
+function solution(n, k) {
+    return n*12000 + k*2000 - parseInt(n/10)*2000
+}
+
+// 인분*12000 + 음료*2000 - (n나누기10의 정수)*2000
+//parseInt() => 정수를 반환한다. ex parseInt(1.234) // 1
