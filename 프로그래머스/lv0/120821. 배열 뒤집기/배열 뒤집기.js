11cd9db3e6dd1e434e4488686d56077cc4de6651
young-02 <75434044+young-02@users.noreply.github.com>
2022-11-14 17:06:41 +0900

    Update 배열 뒤집기.js

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -2,3 +2,21 @@ function solution(num_list) {
 
     return num_list.reverse();
 }
+
+//다른 풀이 보충
+function solution(num_list) {
+    var answer = [];
+    // 앞에서부터  원소 다시 추가
+    num_list.forEach(i => answer.unshift(i))
+    return answer
+}
+
+function solution(num_list) {
+    var answer = [];
+    // 원소의맨 마지막 인덱스 부터 ~ i가 0 같거나 클때 까지 뺀다음 맨뒤부터 새로운 배열에 넣는다.
+    for(let i = num_list.length - 1; i >= 0; i--) {
+        answer.push(num_list[i]);
+    }
+    return answer;
+}
+
