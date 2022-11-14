f7c68729a78d57ad64ac0192242d04ffe5f80b1c
young-02 <75434044+young-02@users.noreply.github.com>
2022-11-14 16:49:02 +0900

    Update 중복된 숫자 개수.js

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -7,3 +7,20 @@ function solution(array, n) {
     },0)
     return answer;
 }
+
+//다른 풀이 보충
+//1.filter
+function solution(array, n) {
+    var answer = 0;
+    let Array = array.filter((item) => item === n).length
+    return answer;
+}
+//2.reduce
+function solution(array, n) {
+    var answer = array.reduce((acc , num) => {
+        if(n === num) acc++
+        return acc
+    } ,0)
+    return answer;
+}
+
