c2cff6aba8045a2501ebfd4daf486680f5ccec06
young-02 <75434044+young-02@users.noreply.github.com>
2022-11-15 20:25:32 +0900

    Update 최댓값 만들기 （1）.js

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -3,3 +3,11 @@ function solution(numbers) {
     var multi = numbers.sort((a,b)=> b-a)
     return multi[0] * multi[1];
 }
+
+
+// sort()
+//오름 차순 정렬 
+arr.sort((a,b)=> a-b)
+//내림 차순 정렬
+arr.sort((a,b)=> b- a))
+
