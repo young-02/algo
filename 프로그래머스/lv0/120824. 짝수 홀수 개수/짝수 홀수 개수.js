66ae0986a8a7b8bf66d6ce992525ac7f19db6f71
young-02 <75434044+young-02@users.noreply.github.com>
2022-11-15 17:40:02 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    var answer = [];
+    let even = 0
+    let odd = 0
+    num_list.reduce((acc,cur)=>{
+      if(cur % 2 === 0){
+          even++
+      }else{
+          odd++
+      }
+    },0)
+    answer.push(even,odd)
+    return answer;
+}
