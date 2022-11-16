36c4cb661e54597af4806387f5405c70fda9481c
young-02 <75434044+young-02@users.noreply.github.com>
2022-11-16 14:45:32 +0900

    Update 문자 반복 출력하기.js

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -2,3 +2,13 @@ function solution(my_string, n) {
     let arr = [...my_string]
     return   arr.map(str => str.repeat(n)).join('')
 }
+
+//repeat 함수
+string.repeat(count)
+
+let num  = '123'
+console.log(num.repeat(2))// '123123'
+
+let arr = ['a','b','c']
+arr.map(str => str.repeat(n))
+console.log(arr.repeat(2))//["aa","bb","cc","dd","ee"]
