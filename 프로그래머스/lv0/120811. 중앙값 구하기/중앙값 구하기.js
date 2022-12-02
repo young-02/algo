73a623d3dd39627db5f09841f7f1a897a753ccb1
young-02 <75434044+young-02@users.noreply.github.com>
2022-12-02 17:57:29 +0900

    Update 중앙값 구하기.js

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,6 +1,8 @@
 function solution(array) {
     let num = array.sort((a,b)=> {return b - a})
-    console.log(num)
-    
     return num[Math.floor(array.length / 2)]
+    
+    
+    //sort 하여서 순서를 오름차순으로 정하고
+    // 베열의 길이를 반으로 나눠서 오름차순으로 만들어둔 배열의 인덱스 값으로 리턴한다.
 }
