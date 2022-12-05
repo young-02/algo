7afeb6ba175942bc3ece805a303d0b0a0d62c989
young-02 <75434044+young-02@users.noreply.github.com>
2022-12-05 18:10:49 +0900

    Update 모음 제거.js

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -8,3 +8,18 @@ function solution(my_string) {
   }
     return answer;
 }
+
+
+
+
+//다른사람 풀이
+
+function solution (my_string){
+    return Array.from(my_string).filter( t => !['a', 'e', 'i', 'o', 'u'].includes(t)).join('')
+}
+
+/*
+  1. filter 로 조건을 만족하면 새로운 배열로 만든다.
+  2. ['a', 'e', 'i', 'o', 'u'] 을 배열로 넣고 'a', 'e', 'i', 'o', 'u' 이 포함 되지 않는것들만 확인하여 includes()로 배열이 특정 요소를 포함하고 있는지 판별
+  3. join('')배열 > 문자열로 만든다.
+*/
