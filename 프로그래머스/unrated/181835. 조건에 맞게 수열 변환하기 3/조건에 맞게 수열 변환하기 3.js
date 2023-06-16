b6249d9f82f7ff8672b7aa9d5ab91910a92f97f6
young <75434044+young-02@users.noreply.github.com>
2023-06-16 11:43:48 +0900

    Update 조건에 맞게 수열 변환하기 3.js

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -9,3 +9,32 @@ function solution(arr, k) {
    }
     return answer;
 }
+
+// 답변2
+function solution(arr,k){
+    arr.map(v => k %2 ? v * k : v + k)
+}
+
+map()메서드는 배열 내의 모든 요소 각각에 대하여 주어진 함수를 호출한 결과를 모아 새로운 배열을 반환합니다.
+
+
+//답변3
+function solution(arr, k){
+    let answer = []
+    
+    for(num of arr ){
+        if(k %2 === 0){
+            answer.push(num + k)
+        }else{
+            answer.push(num* k)
+        }
+    }   
+    return answer
+}
+
+for...of 반복하고 각 개별 속성값에 대해 실행되는 문이 있는 사용자 정의 반복후 후크를 호출하는 루프를 생성합니
+const array1 = ['a','b','c']
+for(const element of array1){
+    console.log(element)
+  // a,b,c
+}
