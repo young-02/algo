1d1dc7ce0192e2e622f103e7c1de2549ed00ecc0
young <75434044+young-02@users.noreply.github.com>
2023-06-16 11:25:40 +0900

    Update 카운트 다운.js

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
@@ -1,10 +1,21 @@
 function solution(start, end) {
     var answer = [];
     for(let i = end; i <= start; i++ ){
-       answer.push(i)
-      
+       answer.push(i)     
     }
       answer.reverse()
-    
     return answer;
 }
+
+
+//방법2
+function solution(start, end) {
+    var answer = [];
+    for(let i = start; i >= end; i-- ){
+       answer.push(i)     
+    }
+    return answer;
+}
+
+end 보다 같거나 클때 까지 start 를 하나씩 뺀다.
+
