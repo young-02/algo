9c754a85f79be21b7c52512abac6b24d4eb17390
young <75434044+young-02@users.noreply.github.com>
2023-07-14 13:17:47 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
@@ -0,0 +1,8 @@
+function solution(num_list, n) {
+    var answer = [];
+    var arr1 = num_list.splice(n)
+    var arr2 = num_list.slice(0,n)
+     answer = arr1.concat(arr2)
+    
+    return answer;
+}
