92fc5ece8f3f9d0b239e5bc8ddc7aa16c5ab8bed
young <75434044+young-02@users.noreply.github.com>
2023-07-14 11:24:52 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -0,0 +1,10 @@
+function solution(a, b) {
+    var answer = 0;
+    let sumA= String(a)+String(b)
+    let sumB = String(b)+String(a)
+
+   return answer = sumA > sumB ? Number(sumA) : Number(sumB)
+   return answer = sumA === sumB ? Number(sumA) : null
+    
+    
+}
