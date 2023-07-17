1db1f573394d9aabb18829cbdc2003ed124cb989
young <75434044+young-02@users.noreply.github.com>
2023-07-17 10:36:21 +0900

    [unrated] Title: 할 일 목록, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -2,13 +2,12 @@ function solution(todo_list, finished) {
    
     const findIdx = []
     
-   for(let i = 0 ; i<finished.length; i++){
+   for(let i = 0 ; i< finished.length; i++){
         if(!finished[i]){ 
-          findIdx.push(i)
+          findIdx.push(todo_list[i])
         }
     }
    
-    const filterIdx = todo_list.filter((item,idx)=> findIdx.includes(idx))
    
-    return filterIdx
+    return findIdx
 }
