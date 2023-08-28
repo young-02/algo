c1e5e12e3d949533229b1c8e29174cffefccac85
young <75434044+young-02@users.noreply.github.com>
2023-08-28 18:40:48 +0900

    [level 2] Title: 주식가격, Time: 29.78 ms, Memory: 42.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.js
@@ -0,0 +1,18 @@
+function solution(prices) {
+    var answer = [];
+    
+    for(let i = 0; i <prices.length; i++){
+        let count =0;
+        
+        for(let j = i+1; j < prices.length; j++){
+            count++
+            
+            if(prices[i] > prices[j]){
+                break;
+            }
+        }
+        
+        answer.push(count)
+    }
+    return answer;
+}
