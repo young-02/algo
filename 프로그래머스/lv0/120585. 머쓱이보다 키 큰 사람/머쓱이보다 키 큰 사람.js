e691cda2e963b0dca2edd6c3395f72d90b113305
young <75434044+young-02@users.noreply.github.com>
2023-08-31 15:19:11 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.12 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,25 +1,7 @@
 function solution(array, height) {
     var answer = 0;
-    array.map(n => {
-        if(n > height){
-            return answer++
-        }
-    })
+    for(let num of array){
+        num > height && answer++ 
+    }
     return answer;
 }
-
-
-//다른사람 풀이 보충
-//1.filter
-function solution(array, height) {
-    var answer = array.filter(item => item > height).length;
-    return answer;
-}
-
-//2.forEach
-function solution(array, height) {
-    var answer = 0;
-    array.forEach(v=> {if(v>height) answer++;})
-    return answer;
-}
-
