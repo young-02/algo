f6793778f0744870c30629f43c4703cc89c6087d
young <75434044+young-02@users.noreply.github.com>
2023-09-04 11:33:09 +0900

    [level 0] Title: 가까운 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,17 +1,3 @@
 function solution(array, n) {
-//1. array의 각 원소와 n과의 절대 거리를 계산하고, 원소와 쌍으로 새 배열에 저장한다.
-	const distance = array.map(el => {
-		return {distance : Math.abs(n-el), value:el}
-	})
-
-	//2. 거리를 기준으로 오름차순 정렬한다. 거리가 같아면 값으로 오름차순을 정렬한다.
-	distance.sort((a,b) => {
-		if(a.distance === b.distance){
-			return a.value - b.value
-		}
-		return a.distance - b.distance
-	})
-
-	// 3. 정렬된 배열의 첫 번째 원소의 value가 n과 가장 가까운 원소이다.
-	return distance[0].value
+ return array.reduce((a,c)=> Math.abs(a-n) < Math.abs(c-n) ? a : Math.abs(a-n) === Math.abs(c-n) ? Math.min(a, c) : c);
 }
