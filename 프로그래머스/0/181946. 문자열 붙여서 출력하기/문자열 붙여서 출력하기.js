9ad21e670d02b630861203740fd54b3b68c49654
young <75434044+young-02@users.noreply.github.com>
2025-02-28 13:45:40 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 53.65 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    let answer = ''
+      for(let text of input){
+          answer += text
+      }
+    console.log(answer)
+    
+});
