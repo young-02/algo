5f437e211dfb7b0a16a4ada9ea0459e816759565
young <75434044+young-02@users.noreply.github.com>
2025-03-12 21:19:44 +0900

    [level 0] Title: 제곱수 판별하기, Time: 2.78 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    for(let i = 1; i<=n; i++){
+        if(i*i === n){
+            return answer = 1
+        }
+        answer = 2
+    }
+    return answer;
+}
